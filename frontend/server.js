b1e14d25607c1e512c58d2fc2b728e5001f7b469
Fpantoja2001 <121701118+Fpantoja2001@users.noreply.github.com>
2024-12-05 17:28:16 -0500

    server routing error fixed

--- frontend/server.js
+++ frontend/server.js
@@ -10,7 +10,12 @@ class Server {
 
     configureMiddleware() {
         // not sure if i should route this to home
-        this.app.use(express.static("../frontend/home"));
+        this.app.use(express.static("../frontend"));
+
+        this.app.get('/', (req, res) => {
+            res.sendFile('home/index.html' , {root:'../frontend/'});
+        });
+        
         this.app.use(express.json({ limit: '10mb' }));
     }
 
