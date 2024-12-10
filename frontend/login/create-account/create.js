f09ae649b2ad95c1765f649644bd9e999d07e9e1
Fpantoja2001 <121701118+Fpantoja2001@users.noreply.github.com>
2024-12-10 08:44:53 -0500

    merge conflict fix

--- frontend/login/create-account/create.js
+++ frontend/login/create-account/create.js
@@ -1,4 +1,5 @@
-import {openDatabase} from "../../model/SQLiteConnection.js";
+import openDatabase from "../../model/SQLiteConnection.js";
+
 const template = document.createElement("template")
 
 const componentTemplates = [
